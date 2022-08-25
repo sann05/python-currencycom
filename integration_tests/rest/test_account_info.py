e1e55c00ce3167cb0bdc32b3cd135099c302d48d
Aliaksandr Sheliutsin <sheliutsin.aliaksandr@gmail.com>
2022-08-25 11:20:22 +0300

    Refactor test_account.py:  - Renamed to test_account_info.py  - Fixed naming for locked assets  - Renamed first test

--- integration_tests/rest/test_account_info.py
+++ integration_tests/rest/test_account_info.py
@@ -1,14 +1,14 @@
 class TestAccount:
-    def test_base(self, client):
+    def test_response_is_not_empty(self, client):
         account_info = client.get_account_info()
         assert len(account_info) > 0
 
     def test_show_balances_without_0(self, client):
         account_info = client.get_account_info(show_zero_balance=False)
-        assert all((balance["free"] + balance["free"]) > 0
+        assert all((balance["free"] + balance["locked"]) > 0
                    for balance in account_info["balances"])
 
     def test_show_balances_with_0(self, client):
         account_info = client.get_account_info(show_zero_balance=True)
-        assert not all((balance["free"] + balance["free"]) > 0
+        assert not all((balance["free"] + balance["locked"]) > 0
                        for balance in account_info["balances"])
