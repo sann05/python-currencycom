05fb61f9f7243f3707fa94281cc42f11c19a5d1d
Trivium0911 <triv_1993@mail.ru>
2022-09-03 10:48:03 +0300

    Added test_order_book.py

---
+++ integration_tests/rest/test_order_book.py
@@ -0,0 +1,33 @@
+import sys
+import pytest
+
+
+class TestOrderBook:
+
+    def test_response_corresponds_swagger_schema(self, client):
+        resp_keys = ['asks', 'bids', 'lastUpdateId']
+        order_book = client.get_order_book('EUR/USD_LEVERAGE')
+        assert type(order_book) is dict
+        assert len(order_book) == 3
+        assert all(dct in resp_keys for dct in order_book.keys())
+        assert all(order_book[key] is not None for key in order_book.keys())
+
+    @pytest.mark.xfail
+    @pytest.mark.parametrize('limit', [5, 10, 20, 50, 100, 500, 1000])
+    def test_limit(self, client, limit):
+        order_book = client.get_order_book('GBP/USD_LEVERAGE', limit=limit)
+        assert len(order_book['asks']) == limit
+        assert len(order_book['bids']) == limit
+
+    @pytest.mark.parametrize('limit', [-sys.maxsize, -1, 0, 1, 506, 999,
+                                       1001, sys.maxsize])
+    def test_invalid_limit(self, client, limit):
+        with pytest.raises(ValueError):
+            client.get_order_book('EUR/USD_LEVERAGE', limit=limit)
+
+    def test_wrong_symbol(self, client):
+        agg_trades = client.get_order_book(symbol="TEST123")
+        assert agg_trades['code'] == -1128 and 'symbol not found ' \
+               in agg_trades['msg']
+
+
