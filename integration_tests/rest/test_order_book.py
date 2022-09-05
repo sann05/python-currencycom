85485684291e2d4d21097805b272cc40b56b7960
Trivium0911 <triv_1993@mail.ru>
2022-09-05 15:53:06 +0300

    Fixed  test_invalid_limit

--- integration_tests/rest/test_order_book.py
+++ integration_tests/rest/test_order_book.py
@@ -18,15 +18,16 @@ class TestOrderBook:
         assert len(order_book['asks']) == limit
         assert len(order_book['bids']) == limit
 
-    @pytest.mark.parametrize('limit', [-sys.maxsize, -1, 0,
-                                       1001, sys.maxsize])
+    @pytest.mark.parametrize('limit', [-sys.maxsize, -1,
+                                       0, sys.maxsize])
     def test_invalid_limit(self, client, limit):
-        with pytest.raises(ValueError):
-            client.get_order_book('EUR/USD_LEVERAGE', limit=limit)
+        order_book = client.get_order_book('EUR/USD_LEVERAGE', limit=limit)
+        assert order_book['code'] == -1128 and order_book['msg'] in \
+               ('Combination of parameters invalid', 'Invalid limit')
 
     def test_wrong_symbol(self, client):
-        agg_trades = client.get_order_book(symbol="TEST123")
-        assert agg_trades['code'] == -1128 and 'symbol not found ' \
-               in agg_trades['msg']
+        order_book = client.get_order_book(symbol="TEST123")
+        assert order_book['code'] == -1128 and 'symbol not found ' \
+               in order_book['msg']
 
 
